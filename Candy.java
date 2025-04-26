2826961e08b6d18e9a9c1908a2f3e81100b3b720
Alessandro-netizen <alessandrom12@nycstudents.net>
2025-04-26 17:35:03 -0400

    Update Candy.java

--- Candy.java
+++ Candy.java
@@ -1,1 +1,16 @@
+public class Candy {
+    private String flavor;
 
+    public Candy(String f) {
+        flavor = f;
+    }
+
+    @Override
+    public String toString() {
+        return flavor;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+}
